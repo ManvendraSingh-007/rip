44ec3172717c9a3971fecc487d41c991d4f205c4
ManvendraSingh-007 <montchouhan@gmail.com>
2026-07-17 12:54:38 +0530

    idk

---
+++ include/rip_archive.hpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <vector>
+#pragma once
+
+void createArchive(const std::string &archivePath,
+                   const std::vector<std::string> &inputPaths);
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "../include/rip_archive.hpp"
+#include <string>
+#include <vector>
+
+int main() {
+  std::string archivePath = "test_env/rip.rip";
+  // std::vector<std::string> inputPaths = {
+  //   "../test_env/1.tiff", "../test_env/2.tiff", "../test_env/3.tiff",
+  //"../test_env/4.tiff", "../test_env/5.tiff"};
+
+  std::vector<std::string> inputPaths = {"test_env/main.py",
+                                         "test_env/game.py"};
+
+  createArchive(archivePath, inputPaths);
+  return 0;
+}
